710f4d956647b62b53ef85494de523b63453e02a
keerthana31999 <keerthanacherry123@gmail.com>
2021-03-25 07:50:10 +0530

    Added::player checking for a option

--- SnakeAndLadderGame.java
+++ SnakeAndLadderGame.java
@@ -8,6 +8,27 @@ public class SnakeAndLadderGame{
 		int player1 = startPoint;
 		Random roll = new Random();
 		int dice = roll.nextInt(6) + 1;
-		System.out.println(dice);
+
+		Random options = new Random();
+		int opt = options.nextInt(3);
+
+		switch(opt){
+			case 0:
+					player1 = player1;
+					System.out.println("Sorry! You can't play and remains at same point");
+					break;
+			case 1:
+					player1 = player1 + dice;
+					System.out.println(" You got a ladder and you have moved " + player1 + " steps forward");
+					break;
+			case 2:
+					player1 = player1 - dice;
+					System.out.println("You are bitten by the snake and moved " + player1 + " steps backward");
+					break;
+			default:
+					System.out.println("invaild");
+					break;
+		}
+	System.out.println(player1);
 	}
 }
