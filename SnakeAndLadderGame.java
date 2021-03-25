91598d783d117756e758aa190e0d23447d164501
keerthana31999 <keerthanacherry123@gmail.com>
2021-03-25 07:55:29 +0530

    Added::player reaching position 100

--- SnakeAndLadderGame.java
+++ SnakeAndLadderGame.java
@@ -6,29 +6,33 @@ public class SnakeAndLadderGame{
 
 		int startPoint = 0;
 		int player1 = startPoint;
-		Random roll = new Random();
-		int dice = roll.nextInt(6) + 1;
 
-		Random options = new Random();
-		int opt = options.nextInt(3);
+		while(player1 <= 100){
+			Random roll = new Random();
+			int dice = roll.nextInt(6) + 1;
 
-		switch(opt){
-			case 0:
-					player1 = player1;
-					System.out.println("Sorry! You can't play and remains at same point");
-					break;
-			case 1:
-					player1 = player1 + dice;
-					System.out.println(" You got a ladder and you have moved " + player1 + " steps forward");
-					break;
-			case 2:
-					player1 = player1 - dice;
-					System.out.println("You are bitten by the snake and moved " + player1 + " steps backward");
-					break;
-			default:
-					System.out.println("invaild");
-					break;
+			Random options = new Random();
+			int opt = options.nextInt(3);
+
+
+			switch(opt){
+				case 0:
+						player1 = player1;
+						break;
+				case 1:
+						player1 = player1 + dice;
+						break;
+				case 2:
+						player1 = player1 - dice;
+						break;
+				default:
+						System.out.println("invaild");
+						break;
+			}
+			   if(player1 < 0){
+				   player1 = startPoint;
+			}
 		}
-	System.out.println(player1);
+		System.out.println("Hurrah! You have reached 100.");
 	}
 }
