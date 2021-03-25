a6866b7ac97a3d90b4d8a416a6899a5036898ae5
keerthana31999 <keerthanacherry123@gmail.com>
2021-03-25 08:06:00 +0530

    Added::position of every dice roll

--- SnakeAndLadderGame.java
+++ SnakeAndLadderGame.java
@@ -5,16 +5,19 @@ public class SnakeAndLadderGame{
 	public static void main(String[] args){
 
 		int startPoint = 0;
-		int player1 = startPoint;
+                int player1 = startPoint;
+		int diceCount = 0;
 
 		while(player1 <= 100){
+
 			Random roll = new Random();
 			int dice = roll.nextInt(6) + 1;
+			diceCount++;
+
 			Random options = new Random();
 			int opt = options.nextInt(3);
 
 			int previousPosition = player1;
-
 			switch(opt){
 				case 0:
 						player1 = player1;
@@ -31,14 +34,17 @@ public class SnakeAndLadderGame{
 			}
 			if(player1 > 100){
 				player1 = previousPosition;
-			}
+                         }
 			if(player1 < 0){
 				player1 = startPoint;
 			}
+
 			if(player1 == 100){
 				break;
 			}
+			System.out.println("You are in " +player1 +" position");
 		}
+		System.out.println("You have rolled the dice for " + diceCount + " times");
 		System.out.println("Hurrah! You have reached 100." );
 	}
 }
