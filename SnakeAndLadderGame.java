37d62adebf95ecf955e181a855c14875fe542abd
keerthana31999 <keerthanacherry123@gmail.com>
2021-03-25 07:46:45 +0530

    Added::rolling the dice

--- SnakeAndLadderGame.java
+++ SnakeAndLadderGame.java
@@ -1,8 +1,13 @@
+import java.util.*;
+
 public class SnakeAndLadderGame{
 
 	public static void main(String[] args){
 
 		int startPoint = 0;
 		int player1 = startPoint;
+		Random roll = new Random();
+		int dice = roll.nextInt(6) + 1;
+		System.out.println(dice);
 	}
 }
